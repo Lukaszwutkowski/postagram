268806e5d3a6fa5b3c0bc795be909c3c8e312547
Lukaszwutkowski <64429535+Lukaszwutkowski@users.noreply.github.com>
2023-03-03 11:27:30 +0100

    Some integrational tests - Update Error Handler

--- src/test/java/com/postagram/controller/LoginControllerTest.java
+++ src/test/java/com/postagram/controller/LoginControllerTest.java
@@ -1,11 +1,13 @@
 package com.postagram.controller;
 
 import com.postagram.dao.UserRepository;
+import com.postagram.error.ApiError;
 import com.postagram.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.http.ResponseEntity;
@@ -34,8 +36,26 @@ public class LoginControllerTest {
 
     @Test
     public void postLogin_withoutUserCredentials_receiveUnauthorized() {
+        authenticate();
         ResponseEntity<Object> response = login(Object.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+    }
+    @Test
+    public void postLogin_withoutUserCredential_receiveErrorHandler() {
+        ResponseEntity<ApiError> response = login(ApiError.class);
+        assertThat(response.getBody().getUrl()).isEqualTo(API_1_0_LOGIN);
+    }
+
+    @Test
+    public void postLogin_withoutUserCredential_receiveUnauthorizedWithoutWWWAuth() {
+        authenticate();
+        ResponseEntity<Object> response = login(Object.class);
+        assertThat(response.getHeaders().containsKey("WW-Authenticate")).isFalse();
+    }
+
+    private void authenticate() {
+        testRestTemplate.getRestTemplate().getInterceptors()
+                .add(new BasicAuthenticationInterceptor("test-user", "P4ssword"));
     }
 
     public <T> ResponseEntity<T> login(Class<T> responseType){
--- src/test/java/com/postagram/dao/UserRepositoryTest.java
+++ src/test/java/com/postagram/dao/UserRepositoryTest.java
@@ -4,28 +4,31 @@ import com.postagram.domain.User;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static com.postagram.controller.UserControllerTest.createValidUser;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
-@DataJpaTest
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestPropertySource(locations = "classpath:application-test.properties")
 @ActiveProfiles("test")
 public class UserRepositoryTest {
-
-    @Autowired
-    TestEntityManager testEntityManager;
 
     @Autowired
     UserRepository userRepository;
 
     @Test
     public void findByUsername_whenUserExists_returnsUser() {
-        testEntityManager.persist(createValidUser());
+
+        createValidUser();
 
         User inDB = userRepository.findByUsername("test-user");
         assertThat(inDB).isNotNull();
@@ -36,5 +39,13 @@ public class UserRepositoryTest {
     public void findByUsername_whenUserDoesNotExist_returnsNull() {
         User inDataBase = userRepository.findByUsername("nonexistinguser");
         assertThat(inDataBase).isNull();
+    }
+
+    public static User createValidUser() {
+        User user = new User();
+        user.setUsername("test-user");
+        user.setDisplayName("test-display");
+        user.setPassword("P4ssword");
+        return user;
     }
 }
